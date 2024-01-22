9a76df42bb874ef1487c1d0b41af21874d92c794
m13corona <mcorona1308@gmail.com>
2024-01-22 14:06:26 -0600

    modify html and javascript files, add/remove code

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+
+var quizQuestions = [{
+    question : "whwhwhw",
+    options : ["mi casa", "la tuya"],
+    answer : "mi casa"
+
+}
+
+]
+
+var nextQuestion = document.getElementById("nextQuestion")
+nextQuestion.addEventListener("click", startQuiz)
+console.log("button clicked")
+
+function startQuiz() {
+//i want to use this to display the first question
+}
+
+
+
+
+//var startQuiz = document.querySelector("#start-quiz");
+
+//var startQuiz = function() { 
+    
+  //  return;
+  ///};
