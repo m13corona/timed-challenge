00a8aa9ae1126550eca07f3c23df21143e553e92
m13corona <mcorona1308@gmail.com>
2024-01-22 22:53:11 -0600

    modified the html, css and javascript files

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 //Here is where I declare the main variables
-var questions = document.querySelector("#questions")
+var questions 
 var timer 
+var initiateQuiz = document.querySelector("#initiate-quiz")
 
 //Here are the questions for the game
 var question1 = {
     question : "What is JavaScript?",
     options: ["a. A software", "b. A variable", "c. A programming language that adds interactivity to a webpage"],
-    answer: question1.options[2]
+    answer: question1.options[2],
 }
 
 var question2 = {
     question : "What is var used for?",
     options : ["a. To declare a variable", "b. to add style in css", "c. it has no functionality"],
-    answer : question2.options[0]
+    answer : question2.options[0],
 }
 
 var question3 = {
@@ -24,12 +25,18 @@ var question3 = {
 
 //We use functions to get the quiz questions started
 function startQuiz() {
-    if (question1 = answer)
-    nextQuestion
+    initiateQuiz 
+
 }
 //add event listener for when we click the button for the next question to be displayed
-var nextQuestion = document.getElementById("nextQuestion")
-nextQuestion.addEventListener("click", startQuiz)
-console.log("button clicked")
+var initiateQuiz = document.getElementById("#initiate-quiz")
+document.addEventListener("click", startQuiz)
 
 
+
+
+
+
+
+//I used the exercises provided in class to help write this code
+//I am missing many lines of code to complete the challenge.
